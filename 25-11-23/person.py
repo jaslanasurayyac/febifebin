70960be958b568424e301d749b168487055b3171
jaslanasurayyac <146112544+jaslanasurayyac@users.noreply.github.com>
2023-12-15 11:28:18 +0530

    Add files via upload

---
+++ 25-11-23/person.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+p1=Person("alice",30)
+p2=Person("minu",20)
+print(p1.name)
+print(p1.age)
+print(p2.name)
+print(p2.age)
+    
